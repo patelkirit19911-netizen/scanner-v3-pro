b52163c133689fde8b771a5f1c3881e33416695d
patelkirit19911-netizen <patelkirit19911@gmail.com>
2026-07-29 17:52:19 +0530

    Add long and short signal functions

--- strategy.py
+++ strategy.py
@@ -1,1 +1,12 @@
+def long_signal(df, previous_day_low, nifty_bullish):
+    """
+    Long signal logic
+    """
+    pass
 
+
+def short_signal(df, previous_day_high, nifty_bearish):
+    """
+    Short signal logic
+    """
+    pass
