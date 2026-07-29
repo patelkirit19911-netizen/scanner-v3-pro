833488468da1454776bfe0719186a0fe035644b4
patelkirit19911-netizen <patelkirit19911@gmail.com>
2026-07-29 17:49:36 +0530

    Add calculate_vwap function to compute VWAP

--- indicators.py
+++ indicators.py
@@ -1,2 +1,7 @@
 import pandas as pd
 from ta.volatility import AverageTrueRange
+def calculate_vwap(df):
+    df = df.copy()
+    tp = (df["high"] + df["low"] + df["close"]) / 3
+    df["vwap"] = (tp * df["volume"]).cumsum() / df["volume"].cumsum()
+    return df
