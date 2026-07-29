be5110c4bf1a71fcf6a09dd142b7044f8207fa61
patelkirit19911-netizen <patelkirit19911@gmail.com>
2026-07-29 18:36:12 +0530

    Implement get_nifty_stocks function
    Added a function to filter stocks from the Nifty 50 index.

--- market_data.py
+++ market_data.py
@@ -12,3 +12,21 @@ dhan = dhanhq(DHAN_CLIENT_ID, DHAN_ACCESS_TOKEN)
 def load_scrip_master():
     df = pd.read_csv(CSV_URL)
     return df
+def get_nifty_stocks():
+    df = load_scrip_master()
+
+    nifty50 = [
+        "RELIANCE","HDFCBANK","ICICIBANK","INFY","TCS","LT",
+        "SBIN","AXISBANK","BHARTIARTL","ITC","KOTAKBANK",
+        "HINDUNILVR","BAJFINANCE","MARUTI","ASIANPAINT",
+        "SUNPHARMA","NTPC","TITAN","ULTRACEMCO","M&M",
+        "POWERGRID","TATAMOTORS","NESTLEIND","HCLTECH",
+        "TECHM","WIPRO","BAJAJFINSV","BAJAJFINANCE","ADANIPORTS",
+        "ADANIENT","JSWSTEEL","TATASTEEL","INDUSINDBK",
+        "CIPLA","GRASIM","EICHERMOT","ONGC","COALINDIA",
+        "HEROMOTOCO","DRREDDY","SHRIRAMFIN","TRENT",
+        "BEL","SBILIFE","BRITANNIA","HDFCLIFE","BPCL",
+        "APOLLOHOSP","BAJAJ-AUTO","HINDALCO"
+    ]
+
+    return df[df["SEM_TRADING_SYMBOL"].isin(nifty50)]
